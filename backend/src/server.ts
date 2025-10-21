1b4abc93f10acb5f2036597754bd8615f38273f0
jacob-nguyen1 <jacobnguyen.dev@gmail.com>
2025-10-20 20:39:16 -0400

    add useless betting route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import healthRouter from "./routes/health";
+import bettingRouter from "./routes/betting";
 
 const app = express();
 
-
 app.use(healthRouter);
+app.use(bettingRouter);
 
 export default app;
