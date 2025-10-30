3ee30253d0931f52c832585888cba02681c5dbec
Saaketh Kesireddy <saakethr.kesireddy@gmail.com>
2025-10-29 20:42:00 -0400

    Added ics reader

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./server";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`api listening on http://localhost:${PORT}`);
