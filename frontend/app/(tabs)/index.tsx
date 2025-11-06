64f37a88a776cc3d391330bff87ef1269125fe68
Lattie <gracemszhao@gmail.com>
2025-11-06 00:10:14 -0500

    Created basic shapes needed for the home screen (and colors). Started creating top menu bar area, with a placeholder pfp right now. Currently hardcoded pfp, but will be replaced with a way to have user upload a pfp (hopefully).

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,28 +1,38 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Text,Dimensions, View, Platform, StyleSheet } from 'react-native';
 
 import { HelloWave } from '@/components/hello-wave';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useFrameSize } from '@react-navigation/elements';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <ThemedView style={styles.stepContainer}>
+    <SafeAreaView style={styles.stepContainer}>
+      <View style={styles.topBar}>
+        # REPLACE WITH SOME WAY TO DO THIS FROM A PIC THE USER UPLOADED
+        <Image 
+          source={require('../../../frontend/assets/images/test_pfp.jpg')}
+          style = {styles.profilePic}
+        />
+        
         <ThemedText>
-          Welcome to the Task-Balancing App!!
-          <HelloWave/>
+          top bar menu area
         </ThemedText>
-        <ThemedText>
-          We are still in development! :P
-        </ThemedText>
-        <ThemedText>
-          <Link href="https://github.com/UF-SASE-Web-Team/Task-Balance-App">Link to Github</Link>
-        </ThemedText>
-      </ThemedView>
+      </View>
+
+      <View style={styles.fieldCircle} />
+
+      <ThemedText>
+          Home screen!!
+      </ThemedText>
     </SafeAreaView>
+    
   );
 }
 
@@ -35,6 +45,9 @@ const styles = StyleSheet.create({
   stepContainer: {
     gap: 8,
     marginBottom: 8,
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#EEFCFF',
   },
   reactLogo: {
     height: 178,
@@ -43,4 +56,33 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+
+  fieldCircle: {
+    width: windowWidth*2,
+    height: windowWidth*2,
+    borderRadius: (windowWidth*2) / 2,
+    backgroundColor: '#AEDF0D',
+    top: windowHeight / 2.3,
+    left: -windowWidth / 2,
+    position: 'absolute',
+    
+  },
+
+  topBar: {
+    width: windowWidth,
+    padding: windowWidth / 20,
+  },
+
+  profilePic: {
+    width: windowWidth/5,
+    height: windowWidth/5,
+    borderRadius: (windowWidth/5) / 2,
+    resizeMode: 'cover',
+  }
 });
+
+/*
+
+
+    
+*/
