f6ae0c7abb27b798b2308ce016c742ec81f7f04e
Lattie <gracemszhao@gmail.com>
2025-11-13 20:34:14 -0500

    test of the button scroll using ScrollView. About to test flatlist. Committing in case I break everything.

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Image } from 'expo-image';
-import { Text,Dimensions, View, Platform, StyleSheet } from 'react-native';
+import { Text,Dimensions, View, Platform, StyleSheet, ScrollView, Pressable, FlatList } from 'react-native';
 
 import { HelloWave } from '@/components/hello-wave';
 import { ThemedText } from '@/components/themed-text';
@@ -8,14 +8,80 @@ import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useFrameSize } from '@react-navigation/elements';
 
+// variables
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
+// dummy variables
+const DATA = [
+  {
+    id: '1',
+    title: 'assignment1',
+    date: 'due date',
+  },
+  {
+    id: '2',
+    title: 'assignment2',
+    date: 'due date',
+  },
+  {
+    id: '3',
+    title: 'assignment3',
+    date: 'due date',
+  },
+  {
+    id: '4',
+    title: 'assignment4',
+    date: 'due date',
+  },
+  {
+    id: '5',
+    title: 'assignment5',
+    date: 'due date',
+  },
+  {
+    id: '6',
+    title: 'assignment6',
+    date: 'due date',
+  },
+  { id: '7',
+    title: 'assignment7',
+    date: 'due date',
+  },
+  {
+    id: '8',
+    title: 'assignment8',
+    date: 'due date',
+  },
+  {
+    id: '9',
+    title: 'assignment9',
+    date: 'due date',
+  },
+  {
+    id: '10',
+    title: 'assignment10',
+    date: 'due date',
+  },
+  { id: '11',
+    title: 'assignment11',
+    date: 'due date',
+  },
+  {
+    id: '12',
+    title: 'assignment12',
+    date: 'due date',
+  }
+]
+
 export default function HomeScreen() {
   return (
+    // step container for whole screen
     <SafeAreaView style={styles.stepContainer}>
+
+      {/* # top menu bar  */}
       <View style={styles.topBar}>
-        # REPLACE WITH SOME WAY TO DO THIS FROM A PIC THE USER UPLOADED
+        {/* # PFP - REPLACE WITH SOME WAY TO DO THIS FROM A PIC THE USER UPLOADED */}
         <Image 
           source={require('../../../frontend/assets/images/test_pfp.jpg')}
           style = {styles.profilePic}
@@ -26,10 +92,26 @@ export default function HomeScreen() {
         </ThemedText>
       </View>
 
-      <View style={styles.fieldCircle} />
+      {/* # adds the field circle (green) */}
+      <View style={styles.fieldCircle}>
+        <ScrollView 
+          showsVerticalScrollIndicator={false} 
+          contentContainerStyle={styles.scrollAssignments}
+          snapToInterval={(windowHeight * 0.1) + 40}
+          decelerationRate="fast"
+          >
+
+          {Array.from({ length: 50 }).map((_, i) => (
+            <View key={i} style = {styles.assignmentButton}>
+              <Text>Assignment {i + 1}</Text>
+              <Text>Some Due Date</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
 
       <ThemedText>
-          Home screen!!
+          Home screen!! yay
       </ThemedText>
     </SafeAreaView>
     
@@ -57,6 +139,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
 
+  // manages the green circle (field) in the BG
   fieldCircle: {
     width: windowWidth*2,
     height: windowWidth*2,
@@ -66,13 +149,37 @@ const styles = StyleSheet.create({
     left: -windowWidth / 2,
     position: 'absolute',
     
+    overflow: 'hidden',
+    alignItems: 'center',
   },
 
+  assignmentButton: {
+    width: windowWidth * 0.4,
+    height: windowHeight * 0.1,
+    borderRadius: 9999,
+    backgroundColor: "#96D022",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+
+  // snap interval: windowHeight * 0.1 + 40 (button height + marginVertical*2)
+
+  // control padding, alignment of the scrollable assignment buttons
+  scrollAssignments: {
+    alignItems: 'center',
+    paddingTop: 50,
+    paddingBottom: 500,
+    width: windowWidth,
+  },
+
+  // manages the top menu bar area
   topBar: {
     width: windowWidth,
     padding: windowWidth / 20,
   },
 
+  // manages the profile picture size and shape
   profilePic: {
     width: windowWidth/5,
     height: windowWidth/5,
